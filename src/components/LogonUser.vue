1376f88632ddbe27fbe60c74487339dcd60279f9
王友冬 <wangyoudong0108@163.com>
2021-01-15 17:16:24 +0800

    登录页面完成

--- src/components/LogonUser.vue
+++ src/components/LogonUser.vue
@@ -1,24 +1,59 @@
 <template>
     <el-form id="logon">
         <h2 style="margin-left: 4%">后台管理系统</h2>
-            <el-form-item >
-                <el-col span="7">
-                <el-input placeholder="请输入用户名"></el-input>
+            <el-form-item :model="user" >
+                <el-col span=7>
+                <el-input placeholder="请输入用户名" v-model="user.name"></el-input>
                 </el-col>
             </el-form-item>
             <el-form-item >
-                <el-col span="7">
-                    <el-input placeholder="请输入密码" show-password></el-input>
+                <el-col span=7>
+                    <el-input placeholder="请输入密码" show-password v-model="user.password"></el-input>
                 </el-col>
             </el-form-item>
-            <el-button type="primary" style="margin-left: 8.5%">立即登录</el-button>
+            <el-button type="primary" style="margin-left: 8.5%" @click="submit">立即登录</el-button>
         </el-form>
 </template>
 
 
 <script>
+    import axios from 'axios';
+    import '../../vue.config'
     export default {
         name: "LogonUser",
+        data(){
+            return{
+                user:{
+                    name:'',
+                    password:''
+                }
+            }
+        },
+        methods:{
+            submit(){
+                    /* json格式提交： */
+                    let formData = JSON.stringify(this.user);
+                    axios({
+                        method:"post",
+                        url:"/verifyUser",
+                        headers: {
+                            "Content-Type": "text/plain"
+                        },
+                        data:formData
+                    }).then((response)=>{
+                        console.log(response);
+                        var r = response.data.toString();
+                        if("index"==r){
+                            this.$alert(r,'友情提示', "success", "success");
+                        }else{
+                            this.$alert(r,'友情提示', "warning","warning");
+                        }
+                    }).catch((response)=>{
+                        this.$alert(response);
+                        //返回index跳转index页面否则提示账户或密码不正确
+                    });
+            }
+        }
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import Logon from "@/Logon";
 import Element from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css'
+ import axios from 'axios'
+
+// axios.defaults.baseURL='/api'
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = '/api'  //关键代码
+Vue.use(axios)
+//提示框
+import VueSimpleAlert from "vue-simple-alert";
+Vue.use(VueSimpleAlert, { reverseButtons: true });
 
 Vue.config.productionTip = false
 Vue.use(Element)
